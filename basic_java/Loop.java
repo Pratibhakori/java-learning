58b1828eb3320631d333618f0ed4e27de0deddd1
Pratibhakori <pratibhakori.7@gmail.com>
2026-02-01 13:34:16 +0530

    first java loop

---
+++ basic_java/Loop.java
@@ -0,0 +1,9 @@
+package basic_java;
+
+public class Loop {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            System.out.println("hello loop");
+        }
+    }
+}
